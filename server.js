21ba9cb71a2115ea978b27a625afc07ac7a9530b
MoisesPH <moisess.pleytez@gmail.com>
2026-07-10 08:08:14 -0600

    df

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
+
 
 // Run it with 
 // npm run dev
@@ -29,6 +32,9 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    console.log(organizations);
+
     const title = 'Our Partner Organizations';
     res.render('organizations', { title });
 });
@@ -45,7 +51,12 @@ app.get('/categories', async (req, res) => {
 
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+    try {
+        await testConnection();
+        console.log(`Server is running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
 });
