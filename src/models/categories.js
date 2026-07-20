27102a3dfa4935e180db986a0076ee4334e39a11
MoisesPH <moisess.pleytez@gmail.com>
2026-07-20 17:47:09 -0600

    fdasfd

--- src/models/categories.js
+++ src/models/categories.js
@@ -9,3 +9,71 @@ export async function getAllCategories() {
         throw error;
     }
 }
+
+export async function getCategoryById(id) {
+    try {
+        const query = `
+SELECT 
+    category_id,
+    name
+FROM 
+    public.project_category
+WHERE 
+    category_id = $1;`;
+        const queryParams = [id];
+        const result = await db.query(query, queryParams);
+        return result.rows[0];
+    } catch (error) {
+        console.error("Error in", error);
+        throw error;
+    }
+}
+
+export async function getCategoriesByProject(projectId) {
+    try {
+        const query = `
+SELECT 
+    pc.category_id,
+    pc.name
+FROM 
+    project_category pc
+INNER JOIN 
+    service_project_category spc ON pc.category_id = spc.category_id
+WHERE 
+    spc.project_id = $1
+ORDER BY 
+    pc.name ASC;`;
+        const queryParams = [projectId];
+        const result = await db.query(query, queryParams);
+        return result.rows;
+    } catch (error) {
+        console.error("Error in", error);
+        throw error;
+    }
+}
+
+export async function getAllProjectsByCategory(categoryId) {
+    try {
+        const query = `SELECT 
+    sp.project_id,
+    sp.organization_id,
+    sp.title,
+    sp.description,
+    sp.location,
+    sp.date
+FROM 
+    service_project sp
+INNER JOIN 
+    service_project_category spc ON sp.project_id = spc.project_id
+WHERE 
+    spc.category_id = $1
+ORDER BY 
+    sp.date ASC;`;
+        const queryParams = [categoryId];
+        const result = await db.query(query, queryParams);
+        return result.rows;
+    } catch (error) {
+        console.error("Error in", error);
+        throw error;
+    }
+}
