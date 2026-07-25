4a545e4290c9c5b55c012c08584ac96a15d56bb0
MoisesPH <moisess.pleytez@gmail.com>
2026-07-25 13:38:47 -0600

    dfafad

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
 import router from './src/routes.js';
+import session from 'express-session';
+import flash from './src/middleware/flash.js';
 
 // Define the application environment
+
+const SESSION_SECRET = process.env.SESSION_SECRET;
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 
 // Define the port number the server will listen on
@@ -22,9 +26,21 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
 /**
   * Configure Express middleware
   */
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // Session expires after 1 hour of inactivity
+}));
+
+// Use flash message middleware
+app.use(flash);
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -25,8 +25,12 @@ export const showNewOrganizationForm = async (req, res) => {
 
 export const processNewOrganizationForm = async (req, res) => {
     const { name, description, contactEmail } = req.body;
-    const logoFilename = 'placeholder-logo.png'; // Use the placeholder logo for all new organizations
+    const logoFilename = 'placeholder-logo.png'; // Use the placeholder logo for all new organizations    
 
     const organizationId = await createOrganization(name, description, contactEmail, logoFilename);
+
+    // Set a success flash message
+    req.flash('success', 'Organization added successfully!');
+
     res.redirect(`/organization/${organizationId}`);
 };
